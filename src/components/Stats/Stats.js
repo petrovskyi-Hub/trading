5aeac03502b40ac6a6086e5e4d41cbd5de5ad16e
Yaroslav Petrivskyi <yarikpetrovsk@gmail.com>
2022-06-15 12:04:25 +0300

    move calcStats to component

---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,59 @@
+import s from "./Stats.module.css";
+
+const calcStats = (periods) => {
+  let counterP = 0;
+  let profitP = 0;
+  let profitN = 0;
+  let counterN = 0;
+
+  for (let period of periods) {
+    if (period.profit > 0) {
+      profitP += Number(period.profit);
+      counterP += 1;
+    } else {
+      profitN += Number(period.profit);
+      counterN += 1;
+    }
+  }
+
+  const sumPercentage = periods.reduce((acc, period) => acc + Number(period.profit), 0);
+  const cleanPercentage = sumPercentage - periods.length * 0.2;
+
+  return (
+    <>
+      <p>
+        <span>Суммарный % всех сделок: </span>
+        <span className={sumPercentage > 0 ? s.positive : s.negative}>
+          {/* {sumPercentage > 0 ? "+" : "-"} */}
+          {sumPercentage.toFixed(0)}
+        </span>
+      </p>
+      <p>
+        <span>Чистая прибыль %: </span>
+        <span className={cleanPercentage > 0 ? s.positive : s.negative}>
+          {/* {sumPercentage > 0 ? "+" : "-"} */}
+          {cleanPercentage.toFixed(0)}
+        </span>
+      </p>
+      <p>
+        <span>Средний % прибыльных/убыточных сделок: </span>
+        <span className={s.positive} style={{ marginRight: "10px" }}>
+          {(profitP / counterP).toFixed(0)}
+        </span>
+        <span className={s.negative}>{counterN !== 0 ? (profitN / counterN).toFixed(0).slice(1) : 0}</span>
+      </p>
+      <p>
+        Общее количество сделок/прибыльных/убыточных: {periods.length}/{counterP}/{counterN}
+      </p>
+      <p>
+        Соотношение сделок в % прибыльных/убыточных: {((counterP / periods.length) * 100).toFixed(0)}/
+        {((counterN / periods.length) * 100).toFixed(0)}
+      </p>
+      <p>
+        Суммарный % убыточных сделок: <span className={s.negative}>{profitN.toFixed(0).slice(1)}</span>
+      </p>
+    </>
+  );
+};
+
+export default calcStats;
